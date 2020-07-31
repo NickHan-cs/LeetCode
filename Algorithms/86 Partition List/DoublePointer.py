ba3bd8ca7fe693aa331ca6adcae46076f631af10
NickHan <chengkaihan1019@outlook.com>
2020-07-31 17:08:27 +0800

    add 86 DoublePointer.py

---
+++ Algorithms/86 Partition List/DoublePointer.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        head1 = ListNode(0)
+        l1 = head1
+        head2 = ListNode(0)
+        l2 = head2
+        while head:
+            if head.val < x:
+                l1.next = head
+                l1 = l1.next
+            else:
+                l2.next = head
+                l2 = l2.next
+            head = head.next
+        l1.next, l2.next = head2.next, None
+        return head1.next
