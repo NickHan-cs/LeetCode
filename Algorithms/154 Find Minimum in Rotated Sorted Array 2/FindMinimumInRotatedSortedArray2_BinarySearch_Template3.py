e7c64e31a2ea88ac373857fa7a501c70c3f0b99e
NickHan <chengkaihan1019@outlook.com>
2020-08-11 10:22:08 +0800

    modify 154 FindMinimumInRotatedSortedArray2_BinarySearch.py to FindMinimumInRotatedSortedArray2_BinarySearch_Template3.py

--- Algorithms/154 Find Minimum in Rotated Sorted Array 2/FindMinimumInRotatedSortedArray2_BinarySearch_Template3.py
+++ Algorithms/154 Find Minimum in Rotated Sorted Array 2/FindMinimumInRotatedSortedArray2_BinarySearch_Template3.py
@@ -1,12 +1,6 @@
-from typing import List
-
-
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l = len(nums)
-        if l == 1 or nums[l - 1] > nums[0]:
-            return nums[0]
-        mid, left, right = 0, 0, l - 1
+        left, right = 0, len(nums) - 1
         while left < right:
             mid = (left + right) // 2
             if nums[mid] > nums[right]:
@@ -16,8 +10,3 @@ class Solution:
             else:
                 right = right - 1
         return nums[left]
-
-
-inList = [2, 2, 0, 1, 2, 2]
-sol = Solution()
-print(sol.findMin(inList))
