32c95097784c409fb74876a158670254a7ec14ae
NickHan <chengkaihan1019@outlook.com>
2020-08-13 16:48:17 +0800

    add 1407 TopTravellers_LeftJoin.sql

---
+++ Database/1407 Top Travellers/TopTravellers_LeftJoin.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select name, ifnull(sum(distance), 0) as travelled_distance
+from Users left join Rides 
+on Users.id = Rides.user_id
+group by name
+order by travelled_distance DESC, name;
